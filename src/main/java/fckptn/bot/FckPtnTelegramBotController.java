c6cd4d37295b8a5fc14eb0e77af9377bd4cde99d
Christian Trutz <info@trutz-software-consulting.de>
2022-03-17 17:27:14 +0100

    Mit Logging auf sysout

--- src/main/java/fckptn/bot/FckPtnTelegramBotController.java
+++ src/main/java/fckptn/bot/FckPtnTelegramBotController.java
@@ -2,13 +2,15 @@ package fckptn.bot;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FckPtnTelegramBotController {
 
 	@PostMapping("/")
-	public ResponseEntity<Void> bot() {
+	public ResponseEntity<Void> bot(@RequestBody String body) {
+		System.out.println(body);
 		return ResponseEntity.ok().build();
 	}
 
