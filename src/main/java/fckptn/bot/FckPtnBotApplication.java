7151719592dece92446ba4fa9153c3230b000dfa
Christian Trutz <info@trutz-software-consulting.de>
2022-03-17 16:07:01 +0100

    Spring Boot Application umbenannt

--- src/main/java/fckptn/bot/FckPtnBotApplication.java
+++ src/main/java/fckptn/bot/FckPtnBotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FckptnBotApplication {
+public class FckPtnBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FckptnBotApplication.class, args);
+		SpringApplication.run(FckPtnBotApplication.class, args);
 	}
 
 }
