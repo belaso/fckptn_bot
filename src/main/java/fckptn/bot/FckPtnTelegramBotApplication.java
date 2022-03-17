49c0dffdafc10e70c13e28824754393e9fad76c9
Christian Trutz <info@trutz-software-consulting.de>
2022-03-17 16:14:43 +0100

    Spring Boot Application umbenannt

--- src/main/java/fckptn/bot/FckPtnTelegramBotApplication.java
+++ src/main/java/fckptn/bot/FckPtnTelegramBotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FckPtnBotApplication {
+public class FckPtnTelegramBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FckPtnBotApplication.class, args);
+		SpringApplication.run(FckPtnTelegramBotApplication.class, args);
 	}
 
 }
