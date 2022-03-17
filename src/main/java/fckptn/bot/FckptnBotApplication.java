98f00344247892345985156893ef49ded032a857
Christian Trutz <info@trutz-software-consulting.de>
2022-03-17 13:53:32 +0100

    Minimales Spring Boot Projekt initialisiert.

---
+++ src/main/java/fckptn/bot/FckptnBotApplication.java
@@ -0,0 +1,13 @@
+package fckptn.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FckptnBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FckptnBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fckptn/bot/FckptnBotApplicationTests.java
@@ -0,0 +1,13 @@
+package fckptn.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FckptnBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
